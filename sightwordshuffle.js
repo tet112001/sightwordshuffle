0407f1f3034a241a7b40e6c52d704033cdfcc86d
Nick Malnick <nmalnick@drw.com>
2017-02-19 12:14:40 -0600

    add green check when word is guessed correctly

--- sightwordshuffle.js
+++ sightwordshuffle.js
@@ -5,12 +5,31 @@ function sightWordsShuffle() {
     $("#shuffleButton").click(function() {
         var sightwordinputs = $('#sightWordsInput').val();
         var sightwords = sightwordinputs.split(" ");
+        var divs = []
         sightwords.forEach(function(word) {
             var shuffledWord = shuffle(word);
             shuffledToSightWord[shuffledWord] = word;
             <!--TODO append input and validation -->
-            $('#shuffledWords').append(shuffledWord);
-        })
+            var wordInputID = word+'-input';
+            var $label = $('<label/>').attr({'id': word+'-label', 'for': wordInputID}).html(shuffledWord);
+            var $input = $('<input/>').attr({'id': wordInputID, 'type': 'text', 'class': 'form-control', 'aria-describedby': wordInputID}).on("input",function(e){    
+                if(word.startsWith($(this).val())) {
+                    $(this).css({'border-color': 'green', 'border-width': 3});
+                    if(word == $(this).val()) {
+                        $(this).hide();
+                        $('#'+word+'-label').html("&nbsp;&nbsp"+word);
+                        var $span = $('<span/>').attr({'class': 'glyphicon glyphicon-ok text-success', 'aria-hidden': 'true'});
+                        $('#'+word+'-label').before($span);
+                    }
+                } else {
+                    $(this).css({'border-color': 'red', 'border-width': 3});
+                };
+            });
+            
+            $('#shuffledWords').append($label, $input);
+        });
+        $('#shuffledWords').append(divs);
+        $('#shuffle-input-group').hide();
     });
     
     function shuffle(word) {
