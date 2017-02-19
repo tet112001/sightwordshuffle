c9e4a20d242a3985bb5373631300bdfca84f0da3
Nick Malnick <nmalnick@drw.com>
2017-02-19 13:13:55 -0600

    added hint

--- sightwordshuffle.js
+++ sightwordshuffle.js
@@ -5,30 +5,32 @@ function sightWordsShuffle() {
     $("#shuffleButton").click(function() {
         var sightwordinputs = $('#sightWordsInput').val();
         var sightwords = sightwordinputs.split(" ");
-        var divs = []
         sightwords.forEach(function(word) {
             var shuffledWord = shuffle(word);
             shuffledToSightWord[shuffledWord] = word;
             <!--TODO append input and validation -->
             var wordInputID = word+'-input';
             var $label = $('<label/>').attr({'id': word+'-label', 'for': wordInputID}).html(shuffledWord);
-            var $input = $('<input/>').attr({'id': wordInputID, 'type': 'text', 'class': 'form-control', 'aria-describedby': wordInputID}).on("input",function(e){    
+            var $input = $('<input/>').attr({'id': wordInputID, 'type': 'text', 'class': 'form-control', 'aria-describedby': wordInputID}).on("input", function(e) {    
                 if(word.startsWith($(this).val())) {
                     $(this).css({'border-color': 'green', 'border-width': 3});
                     if(word == $(this).val()) {
                         $(this).hide();
-                        $('#'+word+'-label').html("&nbsp;&nbsp"+word);
+                        $('#'+word+'-label').html('&nbsp;'+word);
                         var $span = $('<span/>').attr({'class': 'glyphicon glyphicon-ok text-success', 'aria-hidden': 'true'});
-                        $('#'+word+'-label').before($span);
+                        $('#'+word+'-hint').before($span);
+                        $('#'+word+'-label').after('<br/>');
                     }
                 } else {
                     $(this).css({'border-color': 'red', 'border-width': 3});
                 };
             });
-            
-            $('#shuffledWords').append($label, $input);
+            var $hint = $('<span/>').attr({'id': word+'-hint', 'class': 'glyphicon glyphicon-volume-up', 'aria-hidden': 'true'}).click(function() {
+                var msg = new SpeechSynthesisUtterance(word);
+                window.speechSynthesis.speak(msg);
+            });
+            $('#shuffledWords').append($hint, $label, $input);
         });
-        $('#shuffledWords').append(divs);
         $('#shuffle-input-group').hide();
     });
     
