18db646dcf8c9aca5047c179d3080a096cf686e3
Nick Malnick <nmalnick@drw.com>
2017-02-26 18:20:46 -0600

    fixed up input feedback and added hints

--- sightwordshuffle.js
+++ sightwordshuffle.js
@@ -3,33 +3,38 @@ function sightWordsShuffle() {
     var shuffledToSightWord = {};
     
     $("#shuffleButton").click(function() {
-        var sightwordinputs = $('#sightWordsInput').val();
+        var sightwordinputs = $('#sightWordsInput').val().trim().replace(/,/g, ' ').replace(/\s+/g, " ");
+	sightwordinputs = sightwordinputs.replace(/[^a-zA-Z ]+/g, '');
         var sightwords = sightwordinputs.split(" ");
         sightwords.forEach(function(word) {
             var shuffledWord = shuffle(word);
             shuffledToSightWord[shuffledWord] = word;
             var wordInputID = word+'-input';
             var $label = $('<label/>').attr({'id': word+'-label', 'class': 'col-sm-2 control-label', 'for': wordInputID}).html(shuffledWord);
+            
             var $input = $('<input/>').attr({'id': wordInputID, 'type': 'text', 'class': 'form-control', 'aria-describedby': wordInputID}).on("input", function(e) {    
                 if(word.startsWith($(this).val())) {
-                    $(this).css({'border-color': 'green', 'border-width': 3});
                     if(word == $(this).val()) {
-                        $(this).hide();
+                        $('#'+word+'-form-group').removeClass('has-error').addClass('has-success');
                         $('#'+word+'-label').html(word);
-                        var $span = $('<span/>').attr({'class': 'glyphicon glyphicon-ok text-success col-sm-1', 'aria-hidden': 'true'});
-                        $('#'+word+'-hint').before($span);
-                        $('#'+word+'-label').after('<br/>');
+                        var $successSpan = $('<span/>').attr({'class': 'glyphicon glyphicon-ok form-control-feedback', 'aria-hidden': 'true'});
+                        $('#'+word+'-input_group').append($successSpan);
+                        $(this).prop('disabled', true);
                     }
                 } else {
-                    $(this).css({'border-color': 'red', 'border-width': 3});
+                    $('#'+word+'-form-group').removeClass('has-success').addClass('has-error');
+//                    var $errorSpan = $('<span/>').attr({'class': 'glyphicon glyphicon-remove form-control-feedback', 'aria-hidden': 'true'});
+//                    $('#'+word+'-input_group').append($errorSpan);
                 };
             });
-            var $hint = $('<span/>').attr({'id': word+'-hint', 'class': 'glyphicon glyphicon-volume-up col-sm-1', 'aria-hidden': 'true'}).click(function() {
+//        http://getbootstrap.com/css/#forms-control-validation
+            var $hint = $('<span/>').attr({'id': word+'-hint', 'class': 'glyphicon glyphicon-volume-up input-group-addon', 'aria-hidden': 'true'}).click(function() {
                 var msg = new SpeechSynthesisUtterance(word);
                 window.speechSynthesis.speak(msg);
             });
-            var $inputDiv = $('<div/>').attr({'class': 'col-sm-5'}).append($input);
-            var $form = $('<form/>').attr({'class': 'form-group'}).append($hint, $label, $inputDiv);
+            
+            var $inputDiv = $('<div/>').attr({'id': word+'-input_group', 'class': 'input-group col-sm-10'}).append($hint, $input);
+            var $form = $('<div/>').attr({'id': word+'-form-group', 'class': 'form-group has-feedback'}).append($label, $inputDiv);
             $('#shuffledWords').append($form);
         });
         $('#shuffle-input-group').hide();
